c1df2cf4ee6d2803afd70b7a86683a9f75f81d34
Abdullah sajid <abdullahsajid521@gmail.com>
2023-05-06 01:59:42 +0500

    added cors

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require("express")
+const cors = require("cors")
 const port = process.env.PORT ||4000
 const app = express()
+app.use(cors())
 const meme = require("./data.json")
-
 app.get("/",(req,res) => {
-    res.send("meme api") 
+    res.send("<h1>Meme Api</h1>") 
 })
 
 app.get("/meme",(req,res)=>{
     res.send({meme})
 })
 
+
 app.listen(port,() =>{
     console.log("server is running!")
 })
+
