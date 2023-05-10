4379d7c455b84eec1086c830f449196c1beb72bc
Abdullah sajid <abdullahsajid521@gmail.com>
2023-05-11 01:32:41 +0500

    major update

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ const app = express()
 app.use(cors())
 
 const meme = require("./data.json")
-
+const audio = require("./audio.json")
 app.get("/",(req,res) => {
     res.send("<h1>Meme Api</h1>") 
 })
 
-app.get("/meme",(req,res)=>{
+app.get("/meme",(req,res) => {
     res.send({meme})
 })
 
+
+app.get("/audio", (req,res) => {
+    res.send({audio})
+})
 
 app.listen(port,() =>{
     console.log("server is running!")
